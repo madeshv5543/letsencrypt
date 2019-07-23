247b1b8c58fe4e9366106fe2f48a9140ac134a70
madeshv5543 <madeshv5543@gmail.com>
2019-07-23 18:17:39 +0530

    port changes

--- .history/index_20190723181630.js
+++ .history/index_20190723181630.js
@@ -2,12 +2,12 @@ var httpProxy = require('http-proxy');
 var fs = require('fs'); 
 
 httpProxy.createServer({
-         target: {
+          target: {
                       host: '127.0.0.1',
                       port: 6611 
-                  },
+                   },
          ssl: {
                      key: fs.readFileSync('/etc/letsencrypt/live/eventica.smartchain.in/privkey.pem', 'utf8'),
                      cert: fs.readFileSync('/etc/letsencrypt/live/eventica.smartchain.in/fullchain.pem', 'utf8')
-               }
+                  }
 }).listen(443);
