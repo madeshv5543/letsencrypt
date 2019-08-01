84cadd66bd8e93e61216dc7036e6b814519f977e
madeshv5543 <madeshv5543@gmail.com>
2019-08-01 12:16:26 +0530

    handshake changes

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 httpProxy.createServer({
          target: {
                       host: '127.0.0.1',
-                      port: 6611 
+                      port: 6612
                   },
                ws:true,
          ssl: {
-                     key: fs.readFileSync('/etc/letsencrypt/live/eventica.smartchain.in/privkey.pem', 'utf8'),
-                     cert: fs.readFileSync('/etc/letsencrypt/live/eventica.smartchain.in/fullchain.pem', 'utf8')
+                     key: fs.readFileSync('/etc/letsencrypt/live/handshake.in/privkey.pem', 'utf8'),
+                     cert: fs.readFileSync('/etc/letsencrypt/live/handshake.in/fullchain.pem', 'utf8')
                }
 }).listen(443);
